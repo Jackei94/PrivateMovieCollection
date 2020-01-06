8127ceab29f35afa270d118968b98f98a5e0e745
Trammern <55829858+Trammern@users.noreply.github.com>
2020-01-06 12:29:59 +0100

    DatabaseConnector

---
+++ src/dal/DatabaseConnector.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dal;
+
+import com.microsoft.sqlserver.jdbc.SQLServerDataSource;
+import com.microsoft.sqlserver.jdbc.SQLServerException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ *
+ * @author tramm
+ */
+public class DatabaseConnector {
+     private SQLServerDataSource dataSource;
+
+    /**
+     * Constructor for our DatabaseConnector.
+     *
+     * @throws IOException
+     */
+    public DatabaseConnector() throws IOException
+    {
+        Properties props = new Properties();
+        props.load(new FileReader("DBSettings.txt"));
+        dataSource = new SQLServerDataSource();
+        dataSource.setDatabaseName(props.getProperty("database"));
+        dataSource.setUser(props.getProperty("user"));
+        dataSource.setPassword(props.getProperty("password"));
+        dataSource.setServerName(props.getProperty("server"));
+    }
+
+    /**
+     * Gets connection to our database and returns it.
+     *
+     * @return
+     * @throws SQLServerException
+     */
+    public Connection getConnection() throws SQLServerException
+    {
+        return dataSource.getConnection();
+    }
+}
